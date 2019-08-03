04c353ebb006499aa203f13e9a3cc617c7e5cfa7
Andrey Khon <akhon@samodelkin.net>
2019-08-03 16:35:26 +0300

    simple echo server

--- lesson03/client.py
+++ lesson03/client.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python3
 
-import json, subprocess, argparse, os, sys
+import argparse, os, sys
 from socket import *
 
 
-def get_server_time(args):
-    s = socket(AF_INET,SOCK_STREAM)  # Создать сокет TCP
-    s.connect((args.server, args.port))   # Соединиться с сервером
-    tm = s.recv(1024)                # Принять не более 1024 байтов данных
+def craft_message(msg):
+    return msg.encode('utf-8')
+
+
+def send_message(sock, msg):
+    sock.send(craft_message(msg))
+    return
+
+
+def receive_message(s):
+    return parse_message(s.recv(1024))
+
+
+def parse_message(msg):
+    return msg.decode('ascii')
+
+
+def connect(args):
+    s = socket(AF_INET,SOCK_STREAM)
+    s.connect((args.addr, args.port))
+    msg = 'test'
+    send_message(s, msg)
+    t_msg = receive_message(s)
     s.close()
-    print("Current time is {}".format(tm.decode('ascii')))
+    print("Current time is {}".format(t_msg))
 
 
 def main(args):
-    get_server_time(args)
+    connect(args)
 
 
 if __name__ == '__main__':
     # argparser
     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]))
     parser.add_argument('-p','--port',
-        type=int,
-        default=7777,
-        help='Server Connection Port')
-    parser.add_argument('-s','--server',
-        type=str,
-        default='localhost',
-        help='Server Hostname/IP-address')
+                        type=int,
+                        default=7777,
+                        help='Server Connection Port')
+    parser.add_argument('-a','--addr',
+                        type=str,
+                        default='localhost',
+                        help='Server Hostname/IP-address')
     parser.add_argument('-c','--config',
-        type=str,
-        default='config.yml',
-        help='Config File Full Path')
+                        type=str,
+                        default='config.yml',
+                        help='Config File Full Path')
     args = parser.parse_args()
 
     main(args)
--- lesson03/server.py
+++ lesson03/server.py
@@ -1,40 +1,55 @@
 #!/usr/bin/env python
 
-import json, subprocess, argparse, os, sys
+import argparse, os, sys
 from socket import *
-import time
 
 
-def time_server(args):
-    s = socket(AF_INET, SOCK_STREAM)  # Создает сокет TCP
-    s.bind(('', args.port))           # Присваивает порт 8888
-    s.listen(5)                       # Переходит в режим ожидания запросов;
-                                      # одновременно обслуживает не более
-                                      # 5 запросов.
-    print("Time Server on port {} has been created. Awaiting for connections!".format(args.port))
+def craft_message(msg):
+    return msg.encode('utf-8')
 
+
+def send_message(sock, msg):
+    sock.send(craft_message(msg))
+    return
+
+
+def receive_message(sock, addr):
+    msg = sock.recv(1000000).decode('utf-8')
+    print('Received message: {} from client: {}'.format(msg, addr))
+    return msg
+
+
+def echo_server(args):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('', args.port))
+    s.listen(5)
+
+    print('Echo Server on port {} has been created. Awaiting for connections!'.format(args.port))
     while True:
-        client, addr = s.accept()     # Принять запрос на соединение
-        print("Received connection from {}".format(str(addr)))
-        timestr = time.ctime(time.time()) + "\n"
-        client.send(timestr.encode('ascii'))
+        client, addr = s.accept()
+        msg = receive_message(client, addr)
+        send_message(client, msg)
         client.close()
 
 
 def main(args):
-    time_server(args)
+    echo_server(args)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]))
     parser.add_argument('-p','--port',
-        type=int,
-        default=7777,
-        help='Server Connection Port')
+                        type=int,
+                        default=7777,
+                        help='Server Connection Port')
+    parser.add_argument('-a', '--addr',
+                        type=str,
+                        default='config.yml',
+                        help='Server Address')
     parser.add_argument('-c','--config',
-        type=str,
-        default='config.yml',
-        help='Config File Full Path')
+                        type=str,
+                        default='config.yml',
+                        help='Config File Full Path')
     args = parser.parse_args()
 
     main(args)
